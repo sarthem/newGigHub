89e2edbf1c566de6e1508f5f03062bfa09983d83
starman <th.sarigiorgis@gmail.com>
2019-10-16 17:16:20 +0300

    Add project files.

---
+++ newGigHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(newGigHub.Startup))]
+namespace newGigHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
